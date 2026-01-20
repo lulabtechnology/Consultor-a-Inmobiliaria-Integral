108ee9d6b4a1a9fe20d108d38a5a19539165754a
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2026-01-20 13:02:48 -0500

    Create links.ts

---
+++ lib/links.ts
@@ -0,0 +1,14 @@
+export const links = {
+  phoneDisplay: "+507 6474-6565",
+  phoneE164: "50764746565",
+  email: "Clelia@inmobiliaria24panama.com",
+  website: "www.inmobiliaria24panama.com",
+
+  // Puedes reemplazar estos 3 cuando tengas los enlaces exactos
+  instagram: "https://instagram.com/cleliarealtorpn5420",
+  facebook: "https://facebook.com/",
+  linkedin: "https://linkedin.com/in/",
+
+  // WhatsApp Business (usa phoneE164)
+  wa: "https://wa.me/50764746565"
+};
