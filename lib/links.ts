c6467d06e40c47e9faf69756ea560fcf8456107b
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2026-01-24 19:21:12 -0500

    Update links.ts

--- lib/links.ts
+++ lib/links.ts
@@ -7,9 +7,10 @@ export const links = {
 
   website: "www.inmobiliaria24panama.com",
 
-  instagram: "https://instagram.com/cleliarealtorpn5420",
-  facebook: "https://facebook.com/",
-  linkedin: "https://linkedin.com/in/",
+  
+  instagram: "https://www.instagram.com/invites/contact/?utm_source=ig_contact_invite&utm_medium=copy_link&utm_content=rhnjy0c",
+  facebook: "https://www.facebook.com/share/1Dd6s8iyZu/?mibextid=wwXIfr",
+  linkedin: "https://www.linkedin.com/in/clelia-cubilla-16970719b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
 
   wa: "https://wa.me/50764746565"
 };
